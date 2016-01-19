aa158faf601897b5291b952518f2c01728d2d254
xingshaomin <xingshaomin@easemob.com>
2016-01-19 20:33:08 +0800

    commit

--- robot-test/src/main/java/robot/kb/QueryKB.java
+++ robot-test/src/main/java/robot/kb/QueryKB.java
@@ -3,8 +3,8 @@ package robot.kb;
 import robot.GenUtil;
 
 public class QueryKB {
-	private static final String uid = "8207a922984c4ce887a6abcecd123100";
-	private static final String id = "1888f04e19094aee8265fdf401332714";
+	private static final String uid = "5779d0e95784470283ab43c8f2d13f05";
+	private static final String id = "c4bb18aec8ed4249a9a4d2e06402c567";
 	public static void main( String[] args){
 		String curlCommandLine = GenUtil.getCurlCmd(GenUtil.GET, "/kb/custom/" + uid +  "/" + id);
 		System.out.println(curlCommandLine);
